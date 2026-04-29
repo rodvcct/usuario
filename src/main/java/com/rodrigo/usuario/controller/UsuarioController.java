93f110674ce284b4b1568d96fa87a7f950a944d4
Rodrigo <xxrodrigobonfaxx@outlook.com>
2026-04-29 11:09:43 -0300

    adicionado métodos get, delete e login

--- src/main/java/com/rodrigo/usuario/controller/UsuarioController.java
+++ src/main/java/com/rodrigo/usuario/controller/UsuarioController.java
@@ -1,13 +1,16 @@
 package com.rodrigo.usuario.controller;
 
 
+import com.rodrigo.usuario.business.UsuarioService;
 import com.rodrigo.usuario.business.dto.UsuarioDTO;
+import com.rodrigo.usuario.infrastructure.entity.Usuario;
+import com.rodrigo.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -15,9 +18,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UsuarioController {
 
+    private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha())
+        );
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email) {
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
+
     }
 
 }
---
+++ src/main/java/com/rodrigo/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.rodrigo.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
